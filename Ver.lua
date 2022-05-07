94908afbb09dc879cdc9981dee720b18b41a6ccd
David J <8024927+Skyamoeba@users.noreply.github.com>
2022-05-07 17:05:27 +0100

    Update Ver.lua

--- Ver.lua
+++ Ver.lua
@@ -2,6 +2,16 @@ function MODVersionCheck()
   return 37
   end
 
+-- Test Area Version Checker
 function VersionCheckBatt()
   return 101
   end
+
+-- Battery Version Checker
+function VerCheckBattD()
+  return 100
+  end
+function VerCheckBattP()
+  return "1.0.0"
+  end
+-- End Of Battery Version Checker
