c7109acfc08b25ef61577500c7e4d7dbc7c676b9
David J <8024927+Skyamoeba@users.noreply.github.com>
2022-05-07 21:23:30 +0100

    Update Ver.lua

--- Ver.lua
+++ Ver.lua
@@ -12,9 +12,9 @@ function VersionCheckBatt()
 
 -- Battery Version Checker
 function VerCheckBattD()
-  return 100
+  return 101
   end
 function VerCheckBattP()
-  return "1.0.0"
+  return "1.0.1"
   end
 -- End Of Battery Version Checker
