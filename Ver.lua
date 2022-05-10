9d8f915a2ec20d0e0997e0650b24f7334f648dd0
David J <8024927+Skyamoeba@users.noreply.github.com>
2022-05-10 17:05:36 +0100

    Update Ver.lua

--- Ver.lua
+++ Ver.lua
@@ -45,3 +45,59 @@ function VerCheckFluidP()
   return "1.0.0"
   end
 -- End Fluid Manager Version Checker
+function MODVerD()
+  return 37
+  end
+function MODVerP()
+  return "0.3.7"
+  end
+
+-- Test Area Version Checker
+function VersionCheckBatt()
+  return 101
+  end
+
+-- Battery Manager Version Checker
+function VerCheckBattD()
+  return 100
+  end
+function VerCheckBattP()
+  return "1.0.0"
+  end
+-- End Of Battery Manager Version Checker
+
+-- Drone Manager Version Checker
+function VerCheckDroneD()
+  return 100
+  end
+function VerCheckDroneP()
+  return "1.0.0"
+  end
+-- End Drone Manager Version Checker
+
+-- Machine Manager Version Checker
+function VerCheckMachineD()
+  return 100
+  end
+function VerCheckMachineP()
+  return "1.0.0"
+  end
+-- End Machine Manager Version Checker
+
+-- Fluid Manager Version Checker
+function VerCheckFluidD()
+  return 100
+  end
+function VerCheckFluidP()
+  return "1.0.0"
+  end
+-- End Fluid Manager Version Checker
+
+-- Fluid Manager Version Checker
+function VerCheckGasD()
+  return 100
+  end
+function VerCheckGasP()
+  return "1.0.0"
+  end
+-- End Fluid Manager Version Checker
